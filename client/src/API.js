40415eae5731c9f130c0844b43ad3dea850ee504
Janet Jiang <janetjiang1103@gmail.com>
2023-10-30 15:25:15 -0700

    updating search by movie title fetching function and frontend

--- client/src/API.js
+++ client/src/API.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+//fetching popular movies from Movie API 
 export function fetchPopular() {
     const apiUrl = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
 
@@ -13,5 +14,18 @@ export function fetchPopular() {
         }
     })
 
+}
+
+//Search for movies by their original, translated and alternative titles.
+export function fetchByTitle(title){
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`;
+    const apiKey = process.env.REACT_APP_API_KEY
+    
+    return axios.get(apiUrl, {
+        headers: {
+            Authorization: `Bearer ${apiKey}`,
+            accept: 'application/json',
+        }
+    })
 }
 
--- client/src/components/SearchPage/SearchPage.js
+++ client/src/components/SearchPage/SearchPage.js
@@ -1,26 +1,59 @@
 import React from 'react'
+
 import SearchBar from './SearchBar'
 import Filtering from './Filtering'
+import MovieCard from '../MovieCard'
+import {fetchByTitle} from '../../API'
 import { useState, useEffect } from 'react'
 
 const SearchPage = () => {
   const[title, setTitle] = useState([])
+  const[movies, setMovies] = useState([])
 
   //Fetch api
 //search bar component
 //filtering component
 //movie card
-const onSubmit = (title) =>{
-
-  setTitle(title)
-  
+const onSubmit = (searchTitle) =>{
+  setTitle(searchTitle)
+  console.log("title",title)
+  fetchMoviesByTitle(title)
 }
   
+  const fetchMoviesByTitle = async (title) => {
+    try {
+      const response = await fetchByTitle(title)
+
+      const formattedMovies = response.data.results;
+      setMovies(formattedMovies);
+      console.log("formattedMovies", formattedMovies); // Log the formatted movies
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
+  };
+useEffect(()=>{
+  fetchMoviesByTitle()},[title]) // Call the fetchMoviesByTitle function
+ // The empty dependency array ensures the effect runs once
+
+
   return (
     <>
     <div>SearchPage</div>
+
     <div><SearchBar onSubmit={onSubmit}/></div>
     <div><Filtering/></div>
+    <div>
+    <ul>
+        <div className="movie-list">
+          {movies.map(movie => (
+            <div className="movie-card">
+              <li key={movie.id}><MovieCard movie={movie} /></li>
+            </div>
+          ))}
+        </div>
+      </ul>
+    </div>
+
   </>
   )
 }
