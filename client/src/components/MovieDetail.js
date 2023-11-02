dd1768b148027e85bd9179482d30f1a832c63de7
Janet Jiang <janetjiang1103@gmail.com>
2023-11-02 14:08:10 -0700

    fix: changing migration file name

--- client/src/components/MovieDetail.js
+++ client/src/components/MovieDetail.js
@@ -8,9 +8,10 @@ import Heart from 'react-animated-heart';
 
 const MovieDetail = () => {
   const { id } = useParams();
-
+  
   const [isFavourite, setIsFavourite] = useState('false');
   const [movie, setMovie] = useState({});
+  const [componentHasFetchedData, setComponentHasFetchedData] = useState(false)
   // Make the GET request to fetch popular movies
 
   const handleToggleFavourite = () => {
@@ -24,8 +25,10 @@ const MovieDetail = () => {
         const response = await fetchByID(id)
 
         const formattedMovie = response.data;
+        console.log("response", response.data)
         setMovie(formattedMovie);
         console.log(formattedMovie); // Log the formatted movies
+        setComponentHasFetchedData(true)
       } catch (error) {
         console.error('Error fetching movies:', error);
       }
@@ -37,34 +40,37 @@ const MovieDetail = () => {
   }, [id]); // The empty dependency array ensures the effect runs once
 
   let url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-  let genres = movie.genres;
+  console.log("movie", movie)
+  let genres = movie.genres || [];
 
-
+ console.log(componentHasFetchedData)
+  if(!componentHasFetchedData) {
+    return (<div> Loading ... </div>)
+  }
 
   return (
     <>
       <div className='movieDetail'>
         <Card>
           <Card.Body >
-          <Card.Img variant="top" src={url} style={{ width: '20%', height: '20%' }} />
-          <Heart isClick={isFavourite} onClick={handleToggleFavourite} >Favourite</Heart>
-          <div style={{ display: "flex" ,paddingLeft: '10px' }}>
-            <Card.Title>{movie.title}</Card.Title>
-            
-              </div>
+            <Card.Img variant="top" src={url} style={{ width: '20%', height: '20%' }} />
+            <Heart isClick={isFavourite} onClick={handleToggleFavourite} >Favourite</Heart>
+            <div style={{ display: "flex", paddingLeft: '10px' }}>
+              <Card.Title>{movie.title}</Card.Title>
+            </div>
             <Card.Text>
               <div>
                 <span>Movie Description: </span>
                 <div className="new-line-description">{movie.overview}</div>
               </div>
-              {/* <div className="movie-category">
+              <div className="movie-category">
               <span className="category-label">Category:</span>
               {genres.map(genre => (
                 <div className="genres">
                   <li key={genre.id}> {genre.name}</li>
                 </div>
               ))}
-            </div> */}
+            </div>
             </Card.Text>
 
           </Card.Body>
