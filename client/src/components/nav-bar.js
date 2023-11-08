85aa26341a572c51058b9fd8c83d224987e22e79
Janet Jiang <janetjiang1103@gmail.com>
2023-11-07 23:17:28 -0800

    feat: adding css and nav-bar image

--- client/src/components/nav-bar.js
+++ client/src/components/nav-bar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Link, useParams } from 'react-router-dom';
+import {  Link, } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AuthNav from './auth-nav';
@@ -8,9 +8,10 @@ import AuthNav from './auth-nav';
 const NavBar = () => {
   return (
     <div className="nav-container mb-3">
-      <nav className="navbar navbar-expand-md navbar-light">
+      <nav className="navbar navbar-expand-md navbar-light navbar-with-background"> {/* Add a custom class */}
         <div className="container">
           <div className="navbar-brand logo" /> 
+          <img src = "https://www.figma.com/file/UH2efr4osU6Y5lBSYzlDSr/Movie-website?type=design&node-id=124-479&mode=design&t=qo1aqa1Y3UIBWiuY-4" alt = "Logo"/>
           <Link to = "/search">Search</Link>
           <Link to = "/">Home</Link>
           <Link to = "/recommendation">Recommendation</Link>
