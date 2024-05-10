6fc4e12e23fb1f1eaa12271b11c5aebe45f447cf
Janet Jiang <janetjiang1103@gmail.com>
2024-05-09 23:56:25 -0700

    Book management

---
+++ client/src/components/RecomPage/Recommendation.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import RecomForm from './RecomForm'
+import axios from 'axios';
+import MovieCard from '../MovieCard';
+
+const Recommendation = () => {
+  
+
+  const [RecomData, setRecomData] = useState([]);
+  const [purpose, setPurpose] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //get recommendation movies by fetching the backend using user's input
+  const fetchGPTBackend = (userPurpose) =>{
+    setPurpose(userPurpose)
+    const url = `http://localhost:8080/recommendations?purpose=${purpose}`;
+    axios.get(url)
+      .then((response) => {
+        setRecomData(response.data);
+        setIsLoading(false);
+     // Data is fetched, set isLoading to false
+      }) 
+      .catch((error) => console.error('Error fetching data:', error));
+  }
+
+  //When purpose is defined, call fetchbackend
+const onSubmit=(purpose)=>{
+  setIsLoading(true);
+  if (purpose) {
+    fetchGPTBackend(purpose);
+  }
+}
+
+  return (
+    <><div>
+      <RecomForm onSubmit={onSubmit} />
+    </div><div>
+        {isLoading ? ( // Conditionally render loading gif or recommendation list
+         <img src = "https://cdn.dribbble.com/users/530580/screenshots/4712372/loader.gif" alt = "Loading gif"></img>
+      
+        ) : (
+
+          <><ul>
+            <div className="movie-list">
+              {RecomData.map((movie) => (
+                <div className="movie-card">
+                  <li key={movie.results[0].id}><MovieCard movie={movie.results[0]} /></li>
+                </div>
+              ))}
+            </div>
+          </ul></>
+
+        )}
+      </div></>
+   
+  );
+          };
+
+
+
+
+export default Recommendation
